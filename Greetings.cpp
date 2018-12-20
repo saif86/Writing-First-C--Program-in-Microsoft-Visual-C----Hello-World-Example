c0bc7195efb52154d6cacd9fb3db258c546cf797
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-20 13:09:22 +0800

    Update Greetings.cpp

--- Greetings.cpp
+++ Greetings.cpp
@@ -1,7 +1,21 @@
+/**
+* @file Filename.cpp
+*
+* @brief This code prints "Hello World!" to the
+* standard output display. 
+* (Brief decription of the code file goes here)
+*
+* @author Name of developer
+*
+*/
+
 #include <iostream>
 using namespace std;
+
+// function main begins program execution
 void main()
 {
 	cout << "Hello World!" << endl;
 	system("pause");
 }
+// end main
